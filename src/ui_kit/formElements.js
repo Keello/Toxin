691967634a754b20378f3b21feed98eb5bf2f3c5
Keello <Keello27@yandex.ru>
2020-01-12 17:16:08 +0800

    Added expandable checkbox list

---
+++ src/ui_kit/formElements.js
@@ -0,0 +1,10 @@
+$('.expandable-list').click(function(){
+    if($('.expandable-list__items').is(':visible')){
+        $('.expandable-list__items').hide(200);
+        $('.expandable-list').removeClass('expandable-list_opened');
+    }
+    if($('.expandable-list__items').is(':hidden')){
+        $('.expandable-list__items').show(200);
+        $('.expandable-list').addClass('expandable-list_opened');
+    }
+});

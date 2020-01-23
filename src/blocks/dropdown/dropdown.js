83939ae0ece11bddb1eb753880b6192f344d636b
Keello <Keello27@yandex.ru>
2020-01-23 20:09:02 +0800

    fixed dropdown

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,11 +1,12 @@
 $(function(){
   $('.dropdown__wrapper').click(function(){
-    if($('.dropdown__submenu').is(':visible')){
-        $('.dropdown__submenu').slideUp(200);
+    var elem = this.nextSibling;
+    if($(elem).is(':visible')){
+        $(elem).slideUp(200);
         //$('').removeClass('');
     }
-    if($('.dropdown__submenu').is(':hidden')){
-        $('.dropdown__submenu').slideDown(200);
+    if($(elem).is(':hidden')){
+        $(elem).slideDown(200);
         //$('').addClass('');
     }
   });

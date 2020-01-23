d13188a1993fd2533629b315758ba325c3d0bb97
Keello <Keello27@yandex.ru>
2020-01-23 16:09:32 +0800

    blocks was reworked

--- src/blocks/expandable-list/expandable-list.js
+++ src/blocks/expandable-list/expandable-list.js
@@ -1,3 +1,8 @@
 $('.expandable-list').click(function(){
-  $('.expandable-list__items').toggle(200);
+  if($('.expandable-list__items').is(':visible')){
+    $('.expandable-list__items').slideUp(200);
+  }
+  if($('.expandable-list__items').is(':hidden')){
+    $('.expandable-list__items').slideDown(200);
+  }
 });

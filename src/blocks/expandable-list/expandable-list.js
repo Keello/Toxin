ffe05275baf7f77f0a98b06ea3d6662d428d5e15
Keello <Keello27@yandex.ru>
2020-01-23 16:17:02 +0800

    fixed expandable checkbox list

--- src/blocks/expandable-list/expandable-list.js
+++ src/blocks/expandable-list/expandable-list.js
@@ -1,8 +1,10 @@
 $('.expandable-list').click(function(){
   if($('.expandable-list__items').is(':visible')){
     $('.expandable-list__items').slideUp(200);
+    $('.expandable-list').removeClass('expandable-list_opened')
   }
   if($('.expandable-list__items').is(':hidden')){
     $('.expandable-list__items').slideDown(200);
+    $('.expandable-list').addClass('expandable-list_opened')
   }
 });

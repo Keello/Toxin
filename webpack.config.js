36432ab29fb3145322201f7500815b8554343509
Keello <Keello27@yandex.ru>
2020-06-29 23:10:38 +0800

    learning webpack day 1

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    jquery: './src/js/jquery.min.js',
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin()
+  ]
+}

d231015609e38a18cc9712cbb43a276580ec72a1
Keello <Keello27@yandex.ru>
2020-07-07 22:41:27 +0800

    learning webpack day 2

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,50 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
   entry: {
-    jquery: './src/js/jquery.min.js',
-    main: './src/index.js',
+    jquery: './js/jquery.min.js',
+    main: './index.js',
   },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
+  resolve: {
+    alias: {
+      '@blocks': path.resolve(__dirname, 'src/blocks/'),
+    }
+  },
   plugins: [
     new HTMLWebpackPlugin({
-      template: './src/index.html',
+      template: './test.pug',
     }),
     new CleanWebpackPlugin()
-  ]
+  ],
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[a|c]ss$/,
+        use: ['css-loader','sass-loader']
+      },
+      {
+        test:/\.(ttf|woff|woff2|eot|otf)$/,
+        use:['file-loader']
+      },
+      {
+        test:/\.(png|jpg|svg|gif|)$/,
+        use:['file-loader']
+      },
+      {
+        test: /\.pug$/,
+        use:['pug-loader']
+      },
+
+    ]
+  }
 }

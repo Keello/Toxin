b223d313b70a712fab88232883d171a596cf22b0
Keello <Keello27@yandex.ru>
2020-08-22 21:45:55 +0800

    Block dropdown completely rewrited

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,53 +1,82 @@
-$(function(){
-  $('.dropdown__wrapper').click(function(){
-    let elem = this.nextSibling;
-    let values = $('.dropdown__submenu-item-counter').children('.dropdown__number');
-    let quantity = 0;
-    for (let i = 0; i < values.length; i++) {
-      quantity = quantity + +$(values[i]).val();
-    }
-    if(quantity>0)
-      $(this).children('.dropdown__input').val(quantity);
-    if($(elem).is(':visible')){
-        $(elem).slideUp(200);
-    }
-    if($(elem).is(':hidden')){
-        $(elem).slideDown(200);
-    }
-  });
-  $('.dropdown__counter-button-minus').click(function(){
-    val = $(this.nextSibling).val();
-    val -=1;
-    if(val==0)
-      this.setAttribute('disabled', 'disabled');
-    $(this.nextSibling).val(val);
-  });
-  $('.dropdown__counter-button-plus').click(function(){
-    val = $(this.previousSibling).val();
-    val++;
-    $(this.previousSibling).val(val);
-    if(val>0)
-      $(this.previousSibling.previousSibling).removeAttr('disabled');
-  });
+class Dropdown{
+  constructor(elem){
+    this.$el = elem;
+    this.clickHandler = this.clickHandler.bind(this);
+    this.$el.addEventListener('click', this.clickHandler);
+  }
 
-  $('.dropdown_clear').click(function(){
-    let parent = $(this).closest(".dropdown");
-    $(parent).find('.dropdown__input').val('');
-    parent.find('.dropdown__number').val(0);
-    let minus = parent.find('.dropdown__counter-button-minus');
-    for (let i = 0; i < minus.length; i++) {
-      minus[i].setAttribute('disabled', 'disabled');
-    }
-  });
+  clickHandler(){
+    const {type} = event.target.dataset;
 
-  $('.dropdown_confirm').click(function(){
-    let values = $('.dropdown__submenu-item-counter').children('.dropdown__number');
-    let quantity = 0;
-    for (let i = 0; i < values.length; i++) {
-      quantity = quantity + +$(values[i]).val();
+    if(type === 'backdrop'){
+      this.confirm();
+      this.close();
     }
-    let parent = $(this).closest(".dropdown");
-    if(quantity>0)
-      $(parent).find('.dropdown__input').val(quantity);
-  });
-});
+
+    if(type === 'input'){
+      this.toggle();
+    }
+
+    if(type === 'counter-minus'){
+      let counter = event.target.nextElementSibling;
+      counter.value--;
+      if(counter.value == 0){
+        event.target.setAttribute('disabled', 'disabled');
+      }
+    }
+
+    if(type === 'counter-plus'){
+      let counter = event.target.previousElementSibling;
+      let btnMinus = counter.previousElementSibling;
+      counter.value++;
+      if(counter.value > 0 && btnMinus.hasAttribute('disabled')){
+        btnMinus.removeAttribute('disabled');
+      }
+    }
+
+    if(type === 'clear'){
+      this.clear();
+    }
+    if(type === 'confirm'){
+      this.confirm();
+    }
+  }
+
+  open(){
+    this.$el.classList.add('open');
+  }
+
+  close(){
+    this.$el.classList.remove('open');
+  }
+
+  confirm(){
+    event.preventDefault();
+    let sum = 0;
+    let numbers = this.$el.querySelectorAll('.dropdown__number');
+    numbers.forEach(number => sum+= +number.value);
+    this.$el.querySelector('.dropdown__input').value = "Сумма счетчиков: " + sum;
+  }
+
+  clear(){
+    event.preventDefault();
+    let numbers = this.$el.querySelectorAll('.dropdown__number');
+    numbers.forEach(number => {
+      number.value = 0;
+      number.previousElementSibling.setAttribute('disabled', 'disabled');
+    });
+    this.$el.querySelector('.dropdown__input').value = "";
+  }
+
+  get isOpen(){
+    return this.$el.classList.contains('open');
+  }
+
+  toggle(){
+    this.isOpen ? this.close() : this.open();
+  }
+
+}
+
+let elems = document.querySelectorAll('.dropdown');
+elems.forEach(elem => new Dropdown(elem));
